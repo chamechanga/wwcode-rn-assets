1d73d49d70e0adbdbad8a8e6160f678cd1a872b5
Charmaine Andrea Legaspi <charmaineandrealegaspi@gmail.com>
2021-07-23 20:42:30 +0800

    Initial commit

---
+++ data/products.js
@@ -0,0 +1,92 @@
+export default [
+    {
+      id: '0',
+      image: require('../images/clothes/cloth-1.jpeg'),
+      type: 'Tops',
+      name: 'Halter Top',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut imperdiet lectus elit, quis commodo quam viverra vitae. Donec quis mauris turpis. Phasellus vehicula elit in lacus mollis faucibus.',
+      oldPrice: 125,
+      newPrice: 120,
+      totalPrice: 120
+    },
+    {
+      id: '1',
+      image: require('../images/clothes/cloth-2.jpeg'),
+      type: 'Tops',
+      name: 'Tube Top',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut imperdiet lectus elit, quis commodo quam viverra vitae. Donec quis mauris turpis. Phasellus vehicula elit in lacus mollis faucibus.',
+      oldPrice: 54,
+      newPrice: 20,
+      totalPrice: 120
+    },
+    {
+      id: '2',
+      image: require('../images/clothes/cloth-3.jpeg'),
+      type: 'Tops',
+      name: 'T-Shirt',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut imperdiet lectus elit, quis commodo quam viverra vitae. Donec quis mauris turpis. Phasellus vehicula elit in lacus mollis faucibus.',
+      oldPrice: 75,
+      newPrice: 34,
+      totalPrice: 120
+    },
+    {
+      id: '3',
+      image: require('../images/clothes/cloth-4.jpeg'),
+      type: 'Tops',
+      name: 'Sleeveless',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut imperdiet lectus elit, quis commodo quam viverra vitae. Donec quis mauris turpis. Phasellus vehicula elit in lacus mollis faucibus.',
+      oldPrice: 25,
+      newPrice: 20,
+      totalPrice: 120
+    },
+    {
+      id: '4',
+      image: require('../images/clothes/cloth-5.jpeg'),
+      type: 'Tops',
+      name: 'Halter Top',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut imperdiet lectus elit, quis commodo quam viverra vitae. Donec quis mauris turpis. Phasellus vehicula elit in lacus mollis faucibus.',
+      oldPrice: 125,
+      newPrice: 120,
+      totalPrice: 120
+    },
+    {
+      id: '5',
+      image: require('../images/clothes/cloth-6.jpeg'),
+      type: 'Tops',
+      name: 'Tube Top',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut imperdiet lectus elit, quis commodo quam viverra vitae. Donec quis mauris turpis. Phasellus vehicula elit in lacus mollis faucibus.',
+      oldPrice: 54,
+      newPrice: 20,
+      totalPrice: 120
+    },
+    {
+      id: '6',
+      image: require('../images/clothes/cloth-7.jpeg'),
+      type: 'Tops',
+      name: 'T-Shirt',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut imperdiet lectus elit, quis commodo quam viverra vitae. Donec quis mauris turpis. Phasellus vehicula elit in lacus mollis faucibus.',
+      oldPrice: 75,
+      newPrice: 34,
+      totalPrice: 120
+    },
+    {
+      id: '7',
+      image: require('../images/clothes/cloth-8.jpeg'),
+      type: 'Tops',
+      name: 'Sleeveless',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut imperdiet lectus elit, quis commodo quam viverra vitae. Donec quis mauris turpis. Phasellus vehicula elit in lacus mollis faucibus.',
+      oldPrice: 25,
+      newPrice: 20,
+      totalPrice: 120
+    },
+    {
+      id: '8',
+      image: require('../images/clothes/cloth-9.jpeg'),
+      type: 'Tops',
+      name: 'Sleeveless',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut imperdiet lectus elit, quis commodo quam viverra vitae. Donec quis mauris turpis. Phasellus vehicula elit in lacus mollis faucibus.',
+      oldPrice: 25,
+      newPrice: 20,
+      totalPrice: 120
+    },
+  ];
